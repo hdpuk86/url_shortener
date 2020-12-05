d95fcbcbdf1a44940c0e38f2442fd8787553be7b
hdpuk86 <hayley.d.prior@gmail.com>
2020-12-05 21:10:17 +0000

    add basic format validity to long_url

--- app/models/url_connector.rb
+++ app/models/url_connector.rb
@@ -1,4 +1,5 @@
 class UrlConnector < ApplicationRecord
   belongs_to :user
   validates :long_url, presence: true
+  validates :long_url, format: { with: URI::DEFAULT_PARSER.make_regexp(['http', 'https']), message: 'Invalid ' }
 end
